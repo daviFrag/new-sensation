4e3c9ba42da0872d86809d74e9883105728cdd57
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-08-02 14:07:19 +0200

    login form

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FormEventHandler } from "react";
+
 export default function Home() {
-  return <p>Hi</p>;
+  const router = useRouter();
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    // @ts-expect-error
+    const username = event.target.username.value;
+    // @ts-expect-error
+    const password = event.target.password.value;
+    // TODO API
+    console.log(username, password);
+    router.push("/start");
+  };
+
+  return (
+    <form className="flex flex-col px-40 gap-4" onSubmit={onSubmit}>
+      <label htmlFor="username" className="text-left">
+        Username
+      </label>
+      <input id="username" type="text" className="pl-3" />
+      <label htmlFor="password" className="text-left">
+        Password
+      </label>
+      <input id="password" type="password" className="pl-3" />
+      <button
+        type="submit"
+        className="mr-auto border border-black p-2 hover:scale-110"
+      >
+        Login
+      </button>
+    </form>
+  );
 }
