6556583b4271b774ebce60ba16cc5db1d641af39
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-08-04 18:55:50 +0200

    rules css

--- src/app/(main)/rules/RulesLoaded.tsx
+++ src/app/(main)/rules/RulesLoaded.tsx
@@ -11,17 +11,18 @@ export default function RulesLoaded(props: {
 
   return (
     <main>
-      <div>
+      <h1 className="w-11/12 mx-auto text-5xl font-semibold pt-12 pb-5">Regole</h1>
+      <div className="w-11/12 mx-auto text-4xl py-5">
         {vocabularies.map((v) => (
-          <label key={v}>
+          <label key={v} className="flex gap-5">
+            <input type="checkbox" value={v} className="scale-150"/>
             {v}
-            <input type="checkbox" value={v} />
           </label>
         ))}
       </div>
-      <div>
+      <div className="w-11/12 mx-auto">
         {rules.map((r) => (
-          <div key={r.id}>{convertRuleToString(r)}</div>
+          <div key={r.id} className="text-2xl border border-black p-3">{convertRuleToString(r)}</div>
         ))}
       </div>
     </main>
