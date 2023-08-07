5bfa356a1c436c5dc7e2e7183f5477ff239c931e
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-08-07 12:53:50 +0200

    fixed server side rendering problem

--- src/app/(login)/select/page.tsx
+++ src/app/(login)/select/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RedirectButton from "./RedirectButton";
 import { links } from "@/utils/links";
 
 export default function Start() {
   // TODO API
-  const username = localStorage.getItem("username");
+  // const username = localStorage.getItem("username");
+  // ? use client not working, problems with localstorage and pre rendering
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    setUsername(localStorage.getItem("username") ?? "");
+  }, []);
+
   return (
     <>
       <h2 className="text-white">Buongiorno {username}</h2>
