fa6a65e56ed598f1db142deb2aa7bcb103525268
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-08-08 16:48:27 +0200

    fixed header

--- src/app/(main)/Header.tsx
+++ src/app/(main)/Header.tsx
@@ -17,46 +17,46 @@ const links_svgs = {
   tutorial: <Question />,
 } as const;
 
+function SideMenu({ text, hideMenu }: { text: string; hideMenu: () => void }) {
+  return (
+    <aside
+      className="flex flex-col gap-10 text-4xl text-white absolute t-0 l-0 h-screen w-2/7"
+      style={{ backgroundColor: "#146AB9" }}
+    >
+      <div className="h-32 flex items-center px-10">
+        <button onClick={() => hideMenu()} className="h-20 aspect-square">
+          <Hamburger />
+        </button>
+      </div>
+      <nav className="flex flex-col">
+        {Object.keys(links).map((link) => (
+          <a
+            href={link}
+            key={link}
+            className={`flex items-center gap-2 w-full px-10 py-6 ${
+              links[link as keyof typeof links] === text ? "bg-orange-400" : ""
+            } hover:bg-orange-300 ease-in-out duration-75`}
+          >
+            <div className="h-12 w-12">
+              {links_svgs[link as keyof typeof links]}
+            </div>
+            <p className="uppercase">{links[link as keyof typeof links]}</p>
+          </a>
+        ))}
+      </nav>
+    </aside>
+  );
+}
+
 export default function Header({ text }: { text: string }) {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <>
-      {showMenu && (
-        <aside
-          className="flex flex-col gap-10 text-4xl text-white absolute t-0 l-0 h-screen w-2/7"
-          style={{ backgroundColor: "#146AB9" }}
-        >
-          <div className="h-32 flex items-center px-10">
-            <button
-              onClick={() => setShowMenu(false)}
-              className="h-20 aspect-square"
-            >
-              <Hamburger />
-            </button>
-          </div>
-          <nav className="flex flex-col">
-            {Object.keys(links).map((link) => (
-              <a
-                href={link}
-                key={link}
-                className={`flex items-center gap-2 w-full px-10 py-6 ${
-                  links[link as keyof typeof links] === text
-                    ? "bg-orange-400"
-                    : ""
-                } hover:bg-orange-300 ease-in-out duration-75`}
-              >
-                <div className="h-12 w-12">
-                  {links_svgs[link as keyof typeof links]}
-                </div>
-                <p className="uppercase">{links[link as keyof typeof links]}</p>
-              </a>
-            ))}
-          </nav>
-        </aside>
-      )}
+      {showMenu && <SideMenu text={text} hideMenu={() => setShowMenu(false)} />}
+
       <header
-        className="h-32 w-full flex items-center px-10 text-6xl text-white"
+        className="h-32 w-full flex items-center px-10 text-6xl text-white fixed top-0 left-0"
         style={{ backgroundColor: "#146AB9" }}
       >
         <button
@@ -68,6 +68,8 @@ export default function Header({ text }: { text: string }) {
         {!showMenu && <h1 className="ml-10 uppercase">{text}</h1>}
         <h2 className="ml-auto">Smarter: the Rulebook</h2>
       </header>
+
+      <div className="h-32 w-full" />
     </>
   );
 }
