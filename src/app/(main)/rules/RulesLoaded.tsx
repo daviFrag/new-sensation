4875ccddf0529d12f39fb42a477cb29c019f9911
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-08-10 15:14:03 +0200

    search bar

--- src/app/(main)/rules/RulesLoaded.tsx
+++ src/app/(main)/rules/RulesLoaded.tsx
@@ -14,6 +14,16 @@ export default function RulesLoaded(props: {
 }) {
   const { rules, vocabularies, blocks } = props;
   const [rule_modify, setRuleModify] = useState("");
+  const [rule_keyword_searched, setRuleKeywordSearched] = useState("");
+
+  function filterSearchedRules(rules: Rule[]): Rule[] {
+    if (!rule_keyword_searched) return rules;
+    return rules.filter((r) =>
+      convertRuleToString(r)
+        .toLocaleLowerCase()
+        .includes(rule_keyword_searched.trim().toLocaleLowerCase())
+    );
+  }
 
   return (
     <main>
@@ -24,11 +34,19 @@ export default function RulesLoaded(props: {
         vocabularies={vocabularies}
         onChange={(choice) => console.log(choice)}
       />
+      <div className="w-11/12 mx-auto my-5">
+        <input
+          placeholder="Cerca..."
+          className="w-1/4 rounded bg-gray-200 p-2"
+          value={rule_keyword_searched}
+          onChange={(e) => setRuleKeywordSearched(e.target.value)}
+        />
+      </div>
       <div className="w-11/12 mx-auto">
-        {rules.map((r) => (
+        {filterSearchedRules(rules).map((r) => (
           <div
             key={r.id}
-            className="text-2xl border border-black p-3 flex flex-col mb-1 rounded"
+            className="text-2xl border border-black p-3 flex flex-col mb-2 rounded"
           >
             <div className=" flex justify-between">
               <p className="w-10/12">{convertRuleToString(r)}</p>
