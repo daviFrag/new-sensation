62af823edcaeea788d1c2cd7aea572958403048a
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-11-30 23:15:43 +0100

    moved component

--- src/app/(main)/games/GameBox.tsx
+++ src/app/(main)/games/GameBox.tsx
@@ -1,56 +1,16 @@
 import Toggle from "@/components/Toggle";
-import useApiQuery from "@/hooks/useApiQuery";
 import { apiDelete, apiGet, apiPost } from "@/services/api";
 import Bin from "@/svg/Bin";
-import Pen from "@/svg/Pen";
 import {
   Rule,
-  RuleJson,
   TaskInfo,
   TaskJson,
   VocabularyMetadata,
 } from "@/types";
-import { convertRuleToString } from "@/utils/convertRuleToString";
-import { convertRuleJsonToRule } from "@/utils/fromApitoAppTypes";
 import wrapApiCallInWaitingSwal from "@/utils/wrapApiCallInWaitingSwal";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import Swal from "sweetalert2";
-
-function RuleBox(props: {
-  rule: RuleJson;
-  vocabularies_metadata: VocabularyMetadata[];
-}) {
-  const { rule, vocabularies_metadata } = props;
-
-  const getRuleName = () => {
-    const res = convertRuleJsonToRule(rule, vocabularies_metadata);
-    if (res.status !== "success")
-      return `Error reading rule with id ${rule.id}`;
-    return convertRuleToString(res.rule);
-  };
-
-  return (
-    <div className="flex border-t-2 border-solid border-black text-2xl w-full px-7">
-      <p className="flex items-center px-7">{getRuleName()}</p>
-      <div
-        className="ml-auto h-16 p-3"
-        onClick={() => {
-          /* TODO api */
-        }}
-      >
-        <Pen />
-      </div>
-      <div
-        className="h-16 p-3"
-        onClick={() => {
-          /* TODO api */
-        }}
-      >
-        <Bin />
-      </div>
-    </div>
-  );
-}
+import RuleBox from "./RuleBox";
 
 export default function GameBox(props: {
   task: TaskJson;
---
+++ src/app/(main)/games/RuleBox.tsx
@@ -0,0 +1,42 @@
+import Bin from '@/svg/Bin';
+import Pen from '@/svg/Pen';
+import { RuleJson, VocabularyMetadata } from '@/types';
+import { convertRuleToString } from '@/utils/convertRuleToString';
+import { convertRuleJsonToRule } from '@/utils/fromApitoAppTypes';
+import React from 'react'
+
+export default function RuleBox(props: {
+  rule: RuleJson;
+  vocabularies_metadata: VocabularyMetadata[];
+}) {
+  const { rule, vocabularies_metadata } = props;
+
+  const getRuleName = () => {
+    const res = convertRuleJsonToRule(rule, vocabularies_metadata);
+    if (res.status !== "success")
+      return `Error reading rule with id ${rule.id}`;
+    return convertRuleToString(res.rule);
+  };
+
+  return (
+    <div className="flex border-t-2 border-solid border-black text-2xl w-full px-7">
+      <p className="flex items-center px-7">{getRuleName()}</p>
+      <div
+        className="ml-auto h-16 p-3"
+        onClick={() => {
+          /* TODO api */
+        }}
+      >
+        <Pen />
+      </div>
+      <div
+        className="h-16 p-3"
+        onClick={() => {
+          /* TODO api */
+        }}
+      >
+        <Bin />
+      </div>
+    </div>
+  );
+}
