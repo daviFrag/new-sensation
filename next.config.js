0761c0a58cc930083c74daaa0af9d6ddc72e84e7
alex cappelletti <cappelle@fbk.eu>
2023-12-15 12:25:48 +0100

    working on cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
 	output: "standalone",
 
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "https://frontend.smartergame.smartcommunitylab.it" }, // replace this your actual origin
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,DELETE,PATCH,POST,PUT",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
+			},
+		];
+	},
+
 	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
 	// trailingSlash: true,
 
@@ -10,9 +31,9 @@ const nextConfig = {
 
 	// Optional: Change the output directory `out` -> `dist`
 	//distDir: 'dist',
-
 };
 
 module.exports = nextConfig
+
 
 
--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -5,7 +5,7 @@ export default function middleware() {
     // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Credentials', "true")
     res.headers.append('Access-Control-Allow-Origin', 'https://backend.smartergame.smartcommunitylab.it/') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT, OPTIONS')
     res.headers.append(
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
