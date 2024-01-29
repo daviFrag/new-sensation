99533b67dfcb7ba0817571a628ea8a7661d074ac
Davide Frageri <davide.frageri@studenti.unitn.it>
2024-01-29 18:25:17 +0100

    add env variables to docker file build time

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
 
 	env: {
 		DEPLOY_URL: process.env.AUTH0_BASE_URL,
+		AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
 		AUTH0_SECRET: process.env.AUTH0_SECRET,
 		AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
 		AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
