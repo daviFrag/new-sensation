c4be499658b64dc7a65fc4ae626b97e7aa672954
Marrocco Simone <91872829+Marrocco-Simone@users.noreply.github.com>
2024-02-22 14:27:38 +0100

    Revert "new component in modern branch - not tested"

--- src/components/Box/GameBox.tsx
+++ src/components/Box/GameBox.tsx
@@ -8,7 +8,6 @@ import waitForConfirmSwal from "@/utils/waitForConfirmSwal";
 import { AddRuleToTaskModal } from "../Modal";
 import { GameBoxTopRow, RuleBox } from ".";
 import { useCustomUserContext } from "@/app/context/userStore";
-import GamificationMode from "./GamificationMode";
 
 export function GameBox(props: {
   task: TaskJson;
@@ -19,7 +18,7 @@ export function GameBox(props: {
   const { task, rules, vocabularies_metadata, reloadData } = props;
   const task_instances_url = `tasks/${task.id}/instances`;
   const modal = useRef<HTMLDialogElement>(null);
-  const { accessToken } = useCustomUserContext();
+  const {accessToken} = useCustomUserContext();
 
   // * only running instances
   const [instances, setInstances] = useState<TaskInfo[]>();
@@ -130,8 +129,6 @@ export function GameBox(props: {
         rules={rules}
         reloadData={reloadData}
       />
-
-      <GamificationMode />
     </div>
   );
 }
